03cff7180608dff67129ae4c4b52aeb62cb6eac5
Matt Hunt <matthewhunt1984@hotmail.com>
2021-03-05 21:10:01 +0000

    Draft pending account

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-archive
 Creating an archive s3 via terraform
+
+Currently configured to create one bucket with a 7 day policy to glacier.
---
+++ archive.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2" #or nearer
+}
+
+resource "aws_s3_bucket" "media-archive-bucket" {
+  bucket = "my-media-archive"
+  acl    = "private" #?authenticated-read? or grant?
+
+  lifecycle_rule {
+    id      = "media"
+    prefix = "media/"
+    enabled = true
+
+    tags = {
+      rule      = "media"
+      autoclean = "true"
+    }
+
+    transition {
+      days          = 7
+      storage_class = "GLACIER"
+    }
+  }
+}
